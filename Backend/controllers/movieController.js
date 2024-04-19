b5bd4af2d3865f9dd0433fdac2e60ee1de74d150
x <xviper821@gmail.com>
2024-04-20 02:48:30 +0530

    user model and movie upload routes created in backend

--- Backend/controllers/movieController.js
+++ Backend/controllers/movieController.js
@@ -1,72 +1,66 @@
 
 const movie = require("../models/movie")
-const uploadMovie = async (req,res)=>{
+const uploadMovie = async (req, res) => {
 
-    console.log("ok")
+  try {
+    const { name, description } = req.body;
+    let imagePath = req.files['image'][0].path
+    let videoPath = req.files['video'][0].path
 
-  
-        try {
-          const {name, description } = req.body;
-         let imagePath = req.files['image'][0].path
-         let videoPath =  req.files['video'][0].path
+    const Movie = new movie({ name, description, image: imagePath, video: videoPath });
+    let data = await Movie.save();
 
-         console.log(typeof(imagePath));
-      
-     
-          const Movie = new movie({ name, description, image:imagePath, video:videoPath });
-          let data = await Movie.save();
+    if (data) {
+      res.status(200).json({ message: 'Movie uploaded successfully' });
+      console.log("ok")
 
-          if(data){
-            res.status(200).json({ message: 'Movie uploaded successfully' });
-            console.log("ok")
+    }
+    else {
+      res.status(302).json({ error: "Somthing went wrong", success: false })
+    }
 
-          }
-          else{
-            res.status(302).json({message: "somthing went wrong"})
-          }     
-         
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ error: 'Internal server error' });
-        }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error', success: false });
+  }
 
 
-
-      }
-
-
-const getMovies = async (req,res)=>{
-let movies = await movie.find();
-res.json(movies);
-}
-
-const deleteMovie = async (req,res)=>{
-let dMovie = await movie.findByIdAndDelete(req.params.id)
-if(dMovie){
-  res.json({messege:"movie deleted successfully",success:true})
-}
-else{
-  res.json({messege:"somthing went wrong", success:false})
-}
 
 }
 
-const updateMovie = async (req,res)=>{
+
+const getMovies = async (req, res) => {
+  let movies = await movie.find();
+  res.json(movies);
+}
+
+const deleteMovie = async (req, res) => {
+  let dMovie = await movie.findByIdAndDelete(req.params.id)
+  if (dMovie) {
+    res.json({ messege: "movie deleted successfully", success: true })
+  }
+  else {
+    res.json({ messege: "somthing went wrong", success: false })
+  }
+
+}
+
+const updateMovie = async (req, res) => {
 
   let uMovie = await movie.findByIdAndUpdate(req.params.id, req.body);
-  if(uMovie){
-res.json({messege:"movie updated successfully", success:true})
+  if (uMovie) {
+    res.json({ messege: "movie updated successfully", success: true })
 
   }
-  else{
-    res.json({messege:"somthing went wrong", success:false})
+  else {
+    res.json({ messege: "somthing went wrong", success: false })
   }
 
 }
 
 
 
-      module.exports = {uploadMovie, getMovies, deleteMovie, updateMovie}
+module.exports = { uploadMovie, getMovies, deleteMovie, updateMovie }
 
 
 
